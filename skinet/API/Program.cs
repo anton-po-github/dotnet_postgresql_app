ab2f38fc12fa59567693a6c9ccd8e393e113a701
AntonPo <myworkinggemail@gmail.com>
2025-02-06 13:31:50 +0200

    updates

---
+++ skinet/API/Program.cs
@@ -0,0 +1,26 @@
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+[assembly: ApiController]
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddDbContext<StoreContext>(x =>
+{
+    x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
